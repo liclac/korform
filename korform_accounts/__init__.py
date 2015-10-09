64e25c3fd907fb097c846bd6cfdf2245c48757ca
uppfinnarn <uppfinnarn@gmail.com>
2015-10-09 18:09:53 +0200

    Admin: Korform_Accounts -> Accounts

---
+++ korform_accounts/__init__.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+class KorformAccountsConfig(AppConfig):
+    name = 'korform_accounts'
+    verbose_name = 'Accounts'
+
+default_app_config = 'korform_accounts.KorformAccountsConfig'
