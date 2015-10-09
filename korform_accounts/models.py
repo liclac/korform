966146542b3c828699588f4b8f13963f81ca75af
uppfinnarn <uppfinnarn@gmail.com>
2015-10-09 17:37:39 +0200

    Presentation

--- korform_accounts/models.py
+++ korform_accounts/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Profile(models.Model):
-    pass
+    def __unicode__(self):
+      usernames = [u.get_full_name() for u in self.users.all()]
+      return u"Profile for {0}".format(u', '.join(usernames))
 
 class User(AbstractUser):
     profile = models.ForeignKey(Profile, related_name='users', on_delete=models.PROTECT, null=True)
+    
+    def get_full_name(self):
+        return u"{0} {1}".format(self.first_name, self.last_name) if self.first_name else \
+            self.email
 
 def create_user_profile(instance, created, raw, **kwargs):
     if raw:
