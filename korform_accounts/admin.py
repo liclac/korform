621b10ba9fe1e8cf7e2361a661be5f29555e69b2
uppfinnarn <uppfinnarn@gmail.com>
2015-10-09 16:40:51 +0200

    Admin form for users

--- korform_accounts/admin.py
+++ korform_accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserChangeForm as BaseUserChangeForm
+from .models import User, Profile
 
-# Register your models here.
+class UserChangeForm(BaseUserChangeForm):
+    class Meta(BaseUserChangeForm.Meta):
+        model = User
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    
+    def get_fieldsets(self, *args, **kwargs):
+        data = super(BaseUserAdmin, self).get_fieldsets(*args, **kwargs)
+        l = list(data)
+        l.insert(2, (u"Profile", {
+            'fields': ('profile',),
+        }))
+        return tuple(l)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
