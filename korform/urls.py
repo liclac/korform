7e28acb107067952cd65584b1874c3281872e714
uppfinnarn <uppfinnarn@gmail.com>
2015-10-09 14:05:39 +0000

    Route names are good

--- korform/urls.py
+++ korform/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
