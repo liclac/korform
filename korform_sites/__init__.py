4ba9ce67a70f404d6e6e75a2b804bc9829137b43
uppfinnarn <uppfinnarn@gmail.com>
2015-10-15 13:40:26 +0200

    Label for korform_sites

---
+++ korform_sites/__init__.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+class KorformSitesConfig(AppConfig):
+    name = 'korform_sites'
+    verbose_name = 'Sites'
+
+default_app_config = 'korform_sites.KorformSitesConfig'
