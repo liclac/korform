eb9f8b4067cbdfa83112e19971f33a0a2239896f
uppfinnarn <uppfinnarn@gmail.com>
2015-10-15 14:26:41 +0200

    Inline editing of site configs

--- korform_sites/admin.py
+++ korform_sites/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.contrib.sites.models import Site
 from .models import SiteConfig
 
-@admin.register(SiteConfig)
-class ConfigAdmin(admin.ModelAdmin):
-    pass
+admin.site.unregister(Site)
+
+class SiteConfigInline(admin.StackedInline):
+    model = SiteConfig
+    inline_classes = ('grp-collapse grp-open',)
+
+@admin.register(Site)
+class SiteAdmin(admin.ModelAdmin):
+    inlines = [SiteConfigInline]
