eb499e24f52a4bcdc634a37fc78842a981d34e4c
uppfinnarn <uppfinnarn@gmail.com>
2015-10-15 14:33:06 +0200

    AppConfig for korform_planning

---
+++ korform_planning/__init__.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+class KorformPlanningConfig(AppConfig):
+    name = 'korform_planning'
+    verbose_name = 'Planning'
+
+default_app_config = 'korform_planning.KorformPlanningConfig'
