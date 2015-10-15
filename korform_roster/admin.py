f8094f8cfff545b162e84483692b542ef8396a03
uppfinnarn <uppfinnarn@gmail.com>
2015-10-15 15:49:10 +0200

    Member filters

--- korform_roster/admin.py
+++ korform_roster/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 from .models import Member
+
+class BirthYearListFilter(admin.SimpleListFilter):
+    title = _(u"Birthyear")
+    parameter_name = 'birthyear'
+    
+    def lookups(self, request, model_admin):
+        years = model_admin.get_queryset(request).dates('birthday', 'year')
+        return [ (year.year, year.year) for year in years ]
+    
+    def queryset(self, request, queryset):
+        return queryset
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('first_name', 'last_name', 'birthday')
+    list_filter = ('group', BirthYearListFilter)
