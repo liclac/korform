c30b9643b5e40b7c0a7a807e7c161ea5cc3ecf31
uppfinnarn <uppfinnarn@gmail.com>
2015-10-20 21:14:02 +0200

    Inline admin for RSVPs

--- korform_roster/admin.py
+++ korform_roster/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
-from .models import Member
+from .models import Member, RSVP
 
 class BirthYearListFilter(admin.SimpleListFilter):
     title = _(u"Birthyear")
@@ -13,8 +13,12 @@ class BirthYearListFilter(admin.SimpleListFilter):
     def queryset(self, request, queryset):
         return queryset
 
+class RSVPInline(admin.StackedInline):
+    model = RSVP
+
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'birthday')
     list_filter = ('group', BirthYearListFilter)
     search_fields = ('first_name', 'last_name')
+    inlines = [RSVPInline]
