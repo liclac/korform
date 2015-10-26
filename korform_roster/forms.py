4407d3a71bf032c216da452a4f03279e9e3b32c6
uppfinnarn <uppfinnarn@gmail.com>
2015-10-26 17:40:54 +0100

    Layout tweaks

--- korform_roster/forms.py
+++ korform_roster/forms.py
@@ -35,8 +35,8 @@ class MemberForm(forms.ModelForm):
         
         self.helper = FormHelper()
         self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-md-3'
-        self.helper.field_class = 'col-md-9'
+        self.helper.label_class = 'col-md-2'
+        self.helper.field_class = 'col-md-10'
         
         layout_fields = [
             Field('first_name', autofocus=True),
