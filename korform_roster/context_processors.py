2a992cebc9f32869d53b4dce55e6cf080506c055
uppfinnarn <uppfinnarn@gmail.com>
2015-10-26 19:04:00 +0100

    Badge on Contacts if you have only members

--- korform_roster/context_processors.py
+++ korform_roster/context_processors.py
@@ -5,6 +5,12 @@ def badge_counts(request):
         if member.get_badge_count(request):
             members_badge += 1
     
+    contacts_badge = 0
+    contacts = request.user.profile.contacts.all()
+    if len(members) != 0 and len(contacts) == 0:
+        contacts_badge = u"!"
+    
     return {
         'members_badge': members_badge,
+        'contacts_badge': contacts_badge,
     }
