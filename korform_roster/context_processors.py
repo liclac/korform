c421da3f35c4198c681d96a641bcdbf8a3b48937
uppfinnarn <uppfinnarn@gmail.com>
2015-10-28 00:24:54 +0100

    Optimization

--- korform_roster/context_processors.py
+++ korform_roster/context_processors.py
@@ -1,20 +1,22 @@
-def badge_counts(request):
+def user_data(request):
     # Logged out users cannot have badge counts
     if not request.user.is_authenticated():
         return {}
     
     members_badge = 0
-    members = request.user.profile.members.filter(site=request.site)
+    members = request.user.profile.members.filter(site=request.site).prefetch_related('site')
     for member in members:
         if member.get_badge_count(request):
             members_badge += 1
     
     contacts_badge = 0
-    contacts = request.user.profile.contacts.filter(site=request.site)
+    contacts = request.user.profile.contacts.filter(site=request.site).prefetch_related('site')
     if len(members) != 0 and len(contacts) == 0:
         contacts_badge = u"!"
     
     return {
+        'my_members': members,
+        'my_contacts': contacts,
         'members_badge': members_badge,
         'contacts_badge': contacts_badge,
     }
