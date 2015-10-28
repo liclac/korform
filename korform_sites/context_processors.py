8cb2a482d0c9d942e8c9440a26c996c9ffe9a362
uppfinnarn <uppfinnarn@gmail.com>
2015-10-28 01:15:07 +0100

    Handle unset term

--- korform_sites/context_processors.py
+++ korform_sites/context_processors.py
@@ -1,5 +1,5 @@
 def site_data(request):
     site_config = request.site.config
     return {
-        'all_groups': site_config.current_term.groups.all(),
+        'all_groups': site_config.current_term.groups.all() if site_config.current_term else [],
     }
