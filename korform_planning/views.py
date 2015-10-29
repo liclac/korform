006688532cb8c4db8841ffb70f9e41525aae64ad
uppfinnarn <uppfinnarn@gmail.com>
2015-10-29 18:54:45 +0100

    cached_property behaves oddly here

--- korform_planning/views.py
+++ korform_planning/views.py
@@ -1,22 +1,18 @@
 from django.views.generic import DetailView
-from django.utils.functional import cached_property
 from .models import Group
 
 class GroupView(DetailView):
     model = Group
     context_object_name = 'group'
     
-    @cached_property
-    def sheet(self):
-        return self.get_sheet()
-    
     def get_context_data(self, **kwargs):
         context = super(GroupView, self).get_context_data(**kwargs)
         
+        sheet = self.get_sheet()
+        columns = sheet.columns.all()
         members = self.object.members.all()
-        columns = self.sheet.columns.all()
         
-        context['sheet'] = self.sheet
+        context['sheet'] = sheet
         context['columns'] = columns
         context['members'] = members
         context['rows'] = [
