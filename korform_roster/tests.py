613c141b9bd96d9e383c1b5bbaf9378c7f02de14
uppfinnarn <uppfinnarn@gmail.com>
2015-10-30 08:51:23 +0100

    Getting some unit tests in here

--- korform_roster/tests.py
+++ korform_roster/tests.py
@@ -1,3 +1,22 @@
+import datetime
 from django.test import TestCase
+from django.contrib.sites.models import Site
+from korform_accounts.models import Profile
+from korform_planning.models import Group
+from .models import Member, RSVP
 
-# Create your tests here.
+class TestMember(TestCase):
+    def setUp(self):
+        self.site = Site.objects.create(domain=u"google.com", name=u"Google")
+        self.group = Group.objects.create(site=self.site, name=u"Group", code=u"g", slug=u"g", sort=u"g")
+        
+        self.profile = Profile.objects.create()
+        self.john_smith = Member.objects.create(
+            site=self.site, profile=self.profile, group=self.group,
+            first_name=u"John", last_name=u"Smith",
+            birthday=datetime.datetime(2000,12,24),
+        )
+    
+    def test_full_name(self):
+        '''get_full_name() should work as expected.'''
+        self.assertEqual(self.john_smith.get_full_name(), u"John Smith")
--- korform_sites/tests.py
+++ korform_sites/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.contrib.sites.models import Site
+from .models import SiteConfig
 
-# Create your tests here.
+class TestSiteConfig(TestCase):
+    def setUp(self):
+        self.site = Site.objects.create(domain=u"google.com", name=u"Google")
+    
+    def test_config_created(self):
+        '''Newly created Sites should have an associated SiteConfig.'''
+        self.assertIsNotNone(self.site.config)
