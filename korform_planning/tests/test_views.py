21bcd7ca497f820ee0008deaa2be2050a4d8a441
uppfinnarn <uppfinnarn@gmail.com>
2015-10-30 20:52:33 +0100

    Tests for GroupView

--- korform_planning/tests/test_views.py
+++ korform_planning/tests/test_views.py
@@ -9,30 +9,41 @@ from korform_planning.views import GroupView
 
 class TestGroupView(TestCase):
     def setUp(self):
-        self.site = Site.objects.create(domain='google.com', name=u"Google")
+        self.site = Site.objects.get(pk=1)
         
         self.profile = Profile.objects.create()
         
         self.group = Group.objects.create(site=self.site, name=u"Group", code=u'g', slug=u'g', sort=u'g')
-        self.member = Member.objects.create(group=self.group, site=self.site, profile=self.profile,
-            first_name=u"John", last_name=u"Smith", birthday=datetime.date(2000, 12, 24))
-        self.member2 = Member.objects.create(group=self.group, site=self.site, profile=self.profile,
-            first_name=u"John", last_name=u"Doe", birthday=datetime.date(2001, 10, 11))
+        self.member = Member.objects.create(
+            group=self.group, site=self.site, profile=self.profile,
+            first_name=u"John", last_name=u"Smith", birthday=datetime.date(2000, 12, 24),
+            extra={ 'key1': u"Smith 1", 'key2': u"Smith 2" }
+        )
+        self.member2 = Member.objects.create(
+            group=self.group, site=self.site, profile=self.profile,
+            first_name=u"John", last_name=u"Doe", birthday=datetime.date(2001, 10, 11),
+            extra={ 'key1': u"Doe 1", 'key2': u"Doe 2" }
+        )
         
         self.term = Term.objects.create(site=self.site, name=u"Test Term")
         self.term.groups=[self.group]
         
         self.form = Form.objects.create(name=u"Test Form")
         self.form.fields = [
-            FormField(key='first_name', field='textfield', label=u"Field 1"),
-            FormField(key='last_name', field='textfield', label=u"Field 2"),
+            FormField(position=0, key='key1', field='textfield', label=u"Field 1"),
+            FormField(position=1, key='key2', field='textfield', label=u"Field 2"),
         ]
+        self.form.save()
         
-        self.sheet = Form.objects.create(name=u"Test Sheet")
+        self.sheet = Sheet.objects.create(name=u"Test Sheet")
         self.sheet.columns = [
-            SheetColumn(key='first_name', label=u"Column 1"),
-            SheetColumn(key='last_name', label=u"Column 2"),
+            SheetColumn(position=0, key='first_name', label=u"First name"),
+            SheetColumn(position=1, key='last_name', label=u"Last name"),
         ]
+        self.sheet.save()
+        
+        self.site.config.current_term = self.term
+        self.site.config.save()
         
         self.client = Client()
     
@@ -40,3 +51,58 @@ class TestGroupView(TestCase):
         res = self.client.get(reverse('group', kwargs={'slug': 'g'}))
         self.assertEqual(200, res.status_code)
         self.assertEqual([self.member2.pk, self.member.pk], [m.pk for m in res.context['members']])
+    
+    def test_no_term(self):
+        self.site.config.current_term = None
+        self.site.config.save()
+        
+        res = self.client.get(reverse('group', kwargs={'slug': 'g'}))
+        self.assertEqual(200, res.status_code)
+        
+        self.assertEqual(2, len(res.context['columns']))
+        self.assertEqual(u"Name", res.context['columns'][0].label)
+        self.assertEqual(u"Birthday", res.context['columns'][1].label)
+        
+        self.assertEqual(u"John Doe", res.context['rows'][0]['columns'][0]['value'])
+        self.assertEqual(u"2001-10-11", res.context['rows'][0]['columns'][1]['value'])
+        
+        self.assertEqual(u"John Smith", res.context['rows'][1]['columns'][0]['value'])
+        self.assertEqual(u"2000-12-24", res.context['rows'][1]['columns'][1]['value'])
+    
+    def test_term_no_form_or_sheet(self):
+        res = self.client.get(reverse('group', kwargs={'slug': 'g'}))
+        self.assertEqual(200, res.status_code)
+        
+        self.assertEqual(2, len(res.context['columns']))
+        self.assertEqual(u"Name", res.context['columns'][0].label)
+        self.assertEqual(u"Birthday", res.context['columns'][1].label)
+    
+    def test_term_form_only(self):
+        self.term.form = self.form
+        self.term.save()
+        
+        res = self.client.get(reverse('group', kwargs={'slug': 'g'}))
+        self.assertEqual(200, res.status_code)
+        
+        self.assertEqual(u"Name", res.context['columns'][0].label)
+        self.assertEqual(u"Birthday", res.context['columns'][1].label)
+        self.assertEqual(u"Field 1", res.context['columns'][2].label)
+        self.assertEqual(u"Field 2", res.context['columns'][3].label)
+        
+        self.assertEqual(u"John Doe", res.context['rows'][0]['columns'][0]['value'])
+        self.assertEqual(u"2001-10-11", res.context['rows'][0]['columns'][1]['value'])
+        self.assertEqual(u"Doe 1", res.context['rows'][0]['columns'][2]['value'])
+        self.assertEqual(u"Doe 2", res.context['rows'][0]['columns'][3]['value'])
+    
+    def test_term_with_sheet(self):
+        self.term.sheet = self.sheet
+        self.term.save()
+        
+        res = self.client.get(reverse('group', kwargs={'slug': 'g'}))
+        self.assertEqual(200, res.status_code)
+        
+        self.assertEqual(u"First name", res.context['columns'][0].label)
+        self.assertEqual(u"Last name", res.context['columns'][1].label)
+        
+        self.assertEqual(u"John", res.context['rows'][0]['columns'][0]['value'])
+        self.assertEqual(u"Doe", res.context['rows'][0]['columns'][1]['value'])
