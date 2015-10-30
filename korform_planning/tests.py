703825f8ad9531192b437742d1f7372b7f4bef41
uppfinnarn <uppfinnarn@gmail.com>
2015-10-30 11:05:23 +0100

    a <-> b swap in asserts

--- korform_planning/tests.py
+++ korform_planning/tests.py
@@ -11,34 +11,34 @@ class TestEvent(TestCase):
     
     def test_subtitle_nothing(self):
         '''Subtitle with no additional info should be blank.'''
-        self.assertEqual(u"", self.event.get_subtitle())
+        self.assertEqual(self.event.get_subtitle(), u"")
     
     def test_subtitle_with_start(self):
         self.event.start = datetime.datetime(2000,12,24,13,37)
-        self.assertEqual(u"Dec 24 13:37", self.event.get_subtitle())
+        self.assertEqual(self.event.get_subtitle(), u"Dec 24 13:37")
     
     def test_subtitle_with_start_date(self):
         self.event.start = datetime.datetime(2000,12,24)
-        self.assertEqual(u"Dec 24", self.event.get_subtitle())
+        self.assertEqual(self.event.get_subtitle(), u"Dec 24")
     
     def test_subtitle_with_start_and_end(self):
         self.event.start = datetime.datetime(2000,12,24,13,37)
         self.event.end = datetime.datetime(2000,12,24,14,47)
-        self.assertEqual(u"Dec 24 13:37 - 14:47", self.event.get_subtitle())
+        self.assertEqual(self.event.get_subtitle(), u"Dec 24 13:37 - 14:47")
     
     def test_subtitle_with_start_and_end_different_days(self):
         self.event.start = datetime.datetime(2000,12,24,13,37)
         self.event.end = datetime.datetime(2000,12,25,14,47)
-        self.assertEqual(u"Dec 24 13:37 - Dec 25 14:47", self.event.get_subtitle())
+        self.assertEqual(self.event.get_subtitle(), u"Dec 24 13:37 - Dec 25 14:47")
     
     def test_subtitle_with_start_and_end_dates(self):
         self.event.start = datetime.datetime(2000,12,24)
         self.event.end = datetime.datetime(2000,12,25)
-        self.assertEqual(u"Dec 24 - Dec 25", self.event.get_subtitle())
+        self.assertEqual(self.event.get_subtitle(), u"Dec 24 - Dec 25")
     
     def test_subtitle_with_subtitle(self):
         self.event.subtitle = u"Subtitle"
-        self.assertEqual(u"Subtitle", self.event.get_subtitle())
+        self.assertEqual(self.event.get_subtitle(), u"Subtitle")
     
     def test_subtitle_with_start_and_subtitle(self):
         self.event.start = datetime.datetime(2000,12,24,13,37)
