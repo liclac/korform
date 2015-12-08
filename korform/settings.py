8a75fa510b78ec0a8e1fa6419a9f9f32ae8ffb91
uppfinnarn <uppfinnarn@gmail.com>
2015-12-08 15:10:01 +0100

    Template overrides in /override/templates

--- korform/settings.py
+++ korform/settings.py
@@ -95,6 +95,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            os.path.join(BASE_DIR, 'override', 'templates'),
             os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
